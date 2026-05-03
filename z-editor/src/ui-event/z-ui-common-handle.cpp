b1d1b042e54141235ad4842f1b54e0300e6954d7
Wang HengXing <javascript5c@gmail.com>
2026-05-03 20:34:51 +0800

    chore: 更新代码.

--- z-editor/src/ui-event/z-ui-common-handle.cpp
+++ z-editor/src/ui-event/z-ui-common-handle.cpp
@@ -3,31 +3,31 @@
 #include <iostream>
 
 bool ZUICommonHandle::onMouseDown(const ZUIEvent& event) {
-    std::cout << "MouseDown: " << event.x << ", " << event.y << std::endl;
+    // std::cout << "MouseDown: " << event.x << ", " << event.y << std::endl;
     return false;
 }
 
 bool ZUICommonHandle::onMouseMove(const ZUIEvent& event) {
-    std::cout << "MouseMove: " << event.x << ", " << event.y << std::endl;
+    // std::cout << "MouseMove: " << event.x << ", " << event.y << std::endl;
     return false;
 }
 
 bool ZUICommonHandle::onMouseUp(const ZUIEvent& event) {
-    std::cout << "MouseUp: " << event.x << ", " << event.y << std::endl;
+    // std::cout << "MouseUp: " << event.x << ", " << event.y << std::endl;
     return false;
 }
 
 bool ZUICommonHandle::onMouseWheel(const ZUIEvent& event) {
-    std::cout << "MouseWheel: " << event.deltaX << ", " << event.deltaY << std::endl;
+    // std::cout << "MouseWheel: " << event.deltaX << ", " << event.deltaY << std::endl;
     return false;
 }
 
 bool ZUICommonHandle::onKeyDown(const ZUIEvent& event) {
-    std::cout << "KeyDown: " << static_cast<int>(event.keyCode) << std::endl;
+    // std::cout << "KeyDown: " << static_cast<int>(event.keyCode) << std::endl;
     return false;
 }
 
 bool ZUICommonHandle::onKeyUp(const ZUIEvent& event) {
-    std::cout << "KeyUp: " << static_cast<int>(event.keyCode) << std::endl;
+    // std::cout << "KeyUp: " << static_cast<int>(event.keyCode) << std::endl;
     return false;
 }
